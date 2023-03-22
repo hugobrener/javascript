046207041a9169237cc2bf57b5887f9a2ac502fd
hugobrener <hbrener82@gmail.com>
2023-03-22 15:36:11 -0300

    atalhos de teclas

--- modelo/script.js
+++ modelo/script.js
@@ -1,1 +1,8 @@
-alert("olá mundo");
+document.addEventListener("keydown", function(e) {
+    if (e.ctrlKey && e.key === "0"){
+        console.log('olha ele')
+    } else {
+        console.log('num deu gau')
+    }
+})
+
